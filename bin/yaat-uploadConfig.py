c3318356f8fab8221c0ba25e904e45753b16c761
Andrew Melo <andrew.melo@gmail.com>
2012-07-11 16:41:05 -0500

    adding upload binary

---
+++ bin/yaat-uploadConfig.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2.6
+
+from CMSYAAT.ConfigCache.ConfigCache import ConfigCache
+
+cacheFile = ConfigCache()
+config = cacheFile.loadConfig('LHEToAODSIM_cfg.py')
+print "got config"
+target = cacheFile.upload(config, 'meloam', 'meloam',
+                    url="http://andrewmelo.cloudant.com:5984",
+                    database="configcache",
+                    )
+print "Target is %s" % target
