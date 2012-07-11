2855441a8725d55b3277895ad0d4032bf6a71dec
Andrew Melo <andrew.melo@gmail.com>
2012-07-11 16:41:53 -0500

    Doing some refactoring

---
+++ src/CMSYAAT/FileCache/SandboxCache.py
@@ -0,0 +1,16 @@
+'''
+Created by Andrew Melo <andrew.melo@gmail.com> on Jul 11, 2012
+
+'''
+
+class SandboxCache(object):
+    '''
+    Handles generating and uploading Sandboxes to UFC
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
